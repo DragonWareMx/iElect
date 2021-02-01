85324b5635bb65d543e78531eff61c18e855dfa0
ElGuslml <ggs11@hotmail.es>
2021-01-31 22:25:14 -0600

    Seccion individual avance

--- app/Http/Controllers/SeccionesController.php
+++ app/Http/Controllers/SeccionesController.php
@@ -77,12 +77,47 @@ class SeccionesController extends Controller
 
         if (!is_null($campana)) {
             $electores = Elector::where('section_id', $id)->where('campaign_id', $campana->id)->get();
+
+            $edades = $electores->map(function ($item, $key) {
+                return Carbon::parse($item->fecha_nac)->diff(Carbon::now())->format('%y');
+            });
+
+            $rango18 = count($this->array_get_range($edades->toArray(), 18, 18));
+            $rango19 = count($this->array_get_range($edades->toArray(), 19, 19));
+            $rango20 = count($this->array_get_range($edades->toArray(), 20, 24));
+            $rango25 = count($this->array_get_range($edades->toArray(), 25, 29));
+            $rango30 = count($this->array_get_range($edades->toArray(), 30, 34));
+            $rango35 = count($this->array_get_range($edades->toArray(), 35, 39));
+            $rango40 = count($this->array_get_range($edades->toArray(), 40, 44));
+            $rango45 = count($this->array_get_range($edades->toArray(), 45, 49));
+            $rango50 = count($this->array_get_range($edades->toArray(), 50, 54));
+            $rango55 = count($this->array_get_range($edades->toArray(), 55, 59));
+            $rango60 = count($this->array_get_range($edades->toArray(), 60, 64));
+            $rango65 = count($this->array_get_range($edades->toArray(), 65, 200));
+
+            $rangos = [
+                '18' => $rango18,
+                '19' => $rango19,
+                '20_24' => $rango20,
+                '25_29' => $rango25,
+                '30_34' => $rango30,
+                '35_39' => $rango35,
+                '40_44' => $rango40,
+                '45_49' => $rango45,
+                '50_54' => $rango50,
+                '55_59' => $rango55,
+                '60_64' => $rango60,
+                '65_mas' => $rango65
+            ];
+
+            $electores = $this->paginate($electores);
         } else {
             $electores = null;
+            $rangos = null;
         }
 
         $seccion = Section::find($id);
-        return view('usuario.seccion', ['datosSec' => $seccion, 'electores' => $electores]);
+        return view('usuario.seccion', ['datosSec' => $seccion, 'electores' => $electores, 'rangos' => $rangos]);
     }
 
     /**
