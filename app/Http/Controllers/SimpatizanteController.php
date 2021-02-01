c8f1978798a0052bd92135bf0494add7c101717b
ElGuslml <ggs11@hotmail.es>
2021-01-31 23:08:11 -0600

    Ya no se que poner xd

--- app/Http/Controllers/SimpatizanteController.php
+++ app/Http/Controllers/SimpatizanteController.php
@@ -16,6 +16,17 @@ use Webpatser\Uuid\Uuid;
 
 class SimpatizanteController extends Controller
 {
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     //
     public function index($uuid)
     {
