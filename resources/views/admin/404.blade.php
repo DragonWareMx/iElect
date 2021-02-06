d15ec9bc6b245f7d2d607487fa640f92fd44c35f
Dulce Gabriela Marín Rendón <dgmarin61@gmail.com>
2021-02-06 14:58:22 -0600

    Página 404 detalles

--- resources/views/admin/404.blade.php
+++ resources/views/admin/404.blade.php
@@ -10,13 +10,13 @@ Error 404
 
 @section('body')
 
-<div class="uk-flex uk-flex-middle uk-animation-fade uk-margin-medium uk-margin-left uk-margin-right uk-margin-remove-bottom" style="border:1px solid #E5E5E5; border-radius: 5px; height: 30rem;">
-    <div class="uk-position-center">
+<div class="uk-flex uk-flex-middle uk-flex-center uk-animation-fade uk-margin-medium uk-margin-left uk-margin-right uk-margin-remove-bottom" style="border:1px solid #E5E5E5; border-radius: 5px; height: 30rem;">
+    <div>
         <div class="uk-padding-small uk-text-center">
             <h3 class="uk-margin-remove uk-text-muted" style="font-size: 24px;">ERROR 404</h3>
             <h1 class="uk-margin-remove uk-text-primary uk-text-bold text404" style="font-size: 160px;">404</h1>
             <p class="uk-margin-remove uhoh" style="font-size: 24px;">¡¡Uh-oh!! La página que buscas no existe...</p>
-            <a href="#" class="button uk-margin-top">Regresar</a>
+            <a href="javascript:history.back(-1);" class="button uk-margin-top">Regresar</a>
         </div>
     </div>
 </div>
